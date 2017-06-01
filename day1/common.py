4e622d721151717a94f0a881a40ea9ba5bc1b628
Disnon <991879363@qq.com>
2017-06-01 15:43:18 +0800

    common

--- day1/common.py
+++ day1/common.py
@@ -1,20 +1,64 @@
-import time
+import os
+import os.path
+import re
+
+rootdir = "D:\GitProjects\PythonWeb\pythonbasis"  # 指明被遍历的文件夹
 
 
-def timer(func):
-    def deco(*args, **kwargs):
-        start_time = time.time()
-        func(*args, **kwargs)
-        end_time = time.time()
-        print(end_time - start_time)
+def search_file(dir):
+    for i in os.listdir(dir):
+        if i.startswith("."): continue
 
-    return deco
-
-
-@timer
-def print_hello(name, age):
-    time.sleep(2)
-    print("hello %s! you are %s year old?" % (name, age))
+        if os.path.isdir(dir + os.sep + i):
+            search_file(dir + os.sep + i)
+        else:
+            if i.endswith("html"):
+                # 替换文本
+                replese_file(dir + os.sep + i)
+            else:
+                continue
 
 
-print_hello("dinson", 10)
+def replese_file(path):
+    print("替换文本：%s" % path)
+    with open(path, "r", encoding="utf-8") as f:
+        lines = f.readlines()
+
+    with open(path, "w", encoding="utf-8") as f_w:
+        for line in lines:
+            if line.strip() != "":
+
+                s = re.search("href=\"./[0-9]{2}day/index.html\"", line)
+
+                if s is not None:
+                    s1 = s.group()
+                    #                 line = line.replace(s1, "href=\"#\"")
+                    print(s1)
+                    f_w.write("%s \n" % line.strip())
+
+
+search_file(rootdir)
+
+# for i in os.listdir(rootdir):
+#     if i.startswith("."): continue
+#
+#     print(os.path.isdir(rootdir + os.sep + i))
+
+
+# print(os.path.isfile(os.path.join(rootdir,i)))
+
+# if i == "index.html":
+#     with open(rootdir + os.sep + i, "r", encoding="utf-8") as f:
+#         lines = f.readlines()
+#
+#         with open(rootdir + os.sep + i, "w", encoding="utf-8") as f_w:
+#             for line in lines:
+#                 if line.strip() != "":
+#
+#                     s = re.search("href=\"./[0-9]{2}day/index.html\"", line)
+#
+#                     if s is not None:
+#                         s1 = s.group()
+#                         line = line.replace(s1, "href=\"#\"")
+#                         print(s1 )
+#                     f_w.write("%s \n"%line.strip())
