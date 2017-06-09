5bee0fa2b0625349324a36b4a8ce6dc87410fd32
Disnon <991879363@qq.com>
2017-06-09 15:24:47 +0800

    common

--- WarPlanes/mian.py
+++ WarPlanes/mian.py
@@ -1,6 +1,7 @@
 import pygame
 from pygame.locals import *
 import time
+import random
 
 
 class HeroPlane(object):
@@ -16,6 +17,8 @@ class HeroPlane(object):
         for bullet in self.bullets:
             bullet.move()
             bullet.display()
+            if bullet.judge_over():
+                self.bullets.remove(bullet)
 
     def fire(self):
         self.bullets.append(Bullet(self.screen, self.x, self.y))
@@ -33,6 +36,61 @@ class HeroPlane(object):
         self.y += 5
 
 
+class EnemyPlane(object):
+    def __init__(self, x, y, screen):
+        self.x = x
+        self.y = y
+        self.screen = screen
+        self.orientation = random.randint(0, 1)  # 0是向右边移动，1是向左边移动
+        self.image = pygame.image.load("./image/enemy0.png")
+        self.bullets = []
+
+    def display(self):
+        self.screen.blit(self.image, (self.x, self.y))
+        for bullet in self.bullets:
+            bullet.move()
+            bullet.display()
+            if bullet.judge_over():
+                self.bullets.remove(bullet)
+
+    def fire(self):
+
+        code = random.randint(1, 100)
+        if code == 8 or code == 88:
+            self.bullets.append(EnemyBullet(self.screen, self.x, self.y))
+
+    def move(self):
+        self.y += 1
+        if self.orientation:
+            self.x -= 5
+            if self.x < 0:
+                self.x = 0
+                self.orientation = 0
+
+        else:
+            self.x += 5
+            if self.x > 429:
+                self.x = 429
+                self.orientation = 1
+
+
+class EnemyBullet(object):
+    def __init__(self, screen, x, y, ):
+        self.x = x + 21
+        self.y = y + 39
+        self.screen = screen
+        self.image = pygame.image.load("./image/bullet1.png")
+
+    def display(self):
+        self.screen.blit(self.image, (self.x, self.y))
+
+    def move(self):
+        self.y += 10
+
+    def judge_over(self):
+        return True if self.y > 852 else False
+
+
 class Bullet(object):
     def __init__(self, screen, x, y, ):
         self.x = x + 41
@@ -44,7 +102,10 @@ class Bullet(object):
         self.screen.blit(self.image, (self.x, self.y))
 
     def move(self):
-        self.y -= 4
+        self.y -= 10
+
+    def judge_over(self):
+        return True if self.y < 100 else False
 
 
 def game_control(hero_temp):
@@ -85,10 +146,18 @@ def main():
 
     hero = HeroPlane(screen)
 
+    enemy = EnemyPlane(10, 10, screen)
+
     while True:
         screen.blit(background, (0, 0))
 
         hero.display()
+
+        enemy.display()
+
+        enemy.move()
+
+        enemy.fire()
 
         pygame.display.update()
 
--- day1/CreatFile.py
+++ day1/CreatFile.py
@@ -1,3 +1,5 @@
-for i in range(1,10):
-    with open("三国演义-%s.txt"%i,"w") as f:
-        f.write("三国演义-%s.txt"%i)
+# for i in range(1,10):
+#     with open("三国演义-%s.txt"%i,"w") as f:
+#         f.write("三国演义-%s.txt"%i)
+
+
