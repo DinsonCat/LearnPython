282b0e5791bc03a9ecb8b585a572ca0efbfe3fef
Disnon <991879363@qq.com>
2017-06-13 14:18:15 +0800

    common

--- day1/CreatFile.py
+++ day1/CreatFile.py
@@ -3,3 +3,17 @@
 #         f.write("三国演义-%s.txt"%i)
 
 
+# set=map(lambda x,y:x+y,[1,2,3],[4,5])
+# for i in set:
+#     print(i)
+
+
+import functools
+
+def showargs(x,y,a):
+    print(a*x+y)
+
+
+a=functools.partial(showargs,1,2)
+
+a(7)
